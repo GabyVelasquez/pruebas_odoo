f03ed2ea6e658066db72fbe925cc9fe6a192d52a
Gaby <gabykv13@hotmail.com>
2021-11-24 18:51:14 -0500

    Módulo para gestionar visitas

--- custom_crm/__manifest__.py
+++ custom_crm/__manifest__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 {
     'name': "custom_crm",
 
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale_management'],
 
     # always loaded
     'data': [
